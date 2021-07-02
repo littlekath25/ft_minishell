1e01cc09e7d237ffdf1ef1b0e552c532e329cce4
Katherine Fu <kfu@f0r4s6.codam.nl>
2021-07-02 11:00:09 +0200

    changes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kfu <kfu@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/05 15:51:53 by kfu           #+#    #+#                 */
-/*   Updated: 2021/06/29 17:45:17 by kfu           ########   odam.nl         */
+/*   Updated: 2021/07/02 10:58:28 by kfu           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-void	add_command(char *line, t_shell *shell)
-{
-	t_tree		*tree;
-	t_command	*new;
-	t_command	*ptr;
 
-	tree = (t_tree *)ft_calloc(1, sizeof(t_tree));
-	new = (t_command *)ft_calloc(1, sizeof(t_command));
-	ptr = tree->commands;
-	while (ptr != NULL)
-		ptr = tree->commands->next;
-	new->command = line;
-	ptr->next = new;
-}
-
-void	read_from_stdin(t_shell *shell)
+void	read_commands(t_shell *shell)
 {
 	char	*line;
 
@@ -50,7 +36,7 @@ int	main(int argc, char **argv, char **env)
 	{
 		shell = (t_shell *)ft_calloc(1, sizeof(t_shell));
 		shell->env = env;
-		read_from_stdin(shell);
+		read_commands(shell);
 		exit(0);
 	}
 	else
